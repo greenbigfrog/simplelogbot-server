73dd49bc8d3c300f4de2a14b86c65af098bec08e
Jonathan B <greenbigfrog@gmail.com>
2016-09-04 16:33:00 +0200

    "Fix" routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,18 @@ Rails.application.routes.draw do
     scope '/v1' do
       put '/status' => 'status#update'
       scope '/servers' do
-        resources :api_server, only: [:create, :update]
+        resources :api_server, path: '/', only: [:create, :update]
         scope '/channels' do
-          resources :api_channel, only: [:create, :update]
+          resources :api_channel, path: '/', only: [:create, :update]
           scope '/messages' do
-            resources :api_message, only: [:create, :update]
+            resources :api_message, path: '/', only: [:create, :update]
           end
         end
       end
       scope '/users' do
-        resources :api_user, only: [:create, :update]
+        resources :api_user, path: '/', only: [:create, :update]
         scope '/memberships' do
-          resources :api_membership, only: [:create, :update]
+          resources :api_membership, path: '/', only: [:create, :update]
         end
       end
     end
