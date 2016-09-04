112ca3a34d927fc804f0f9336431533b9c94a66b
Jonathan B <greenbigfrog@gmail.com>
2016-09-04 20:17:04 +0200

    fix routing and disable devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   scope '/api' do
     scope '/v1' do
       put '/status' => 'status#update'
       scope '/servers' do
         resources :api_server, path: '/', only: [:create, :update]
-        scope '/channels' do
-          resources :api_channel, path: '/', only: [:create, :update]
-          scope '/messages' do
-            resources :api_message, path: '/', only: [:create, :update]
-          end
-        end
+      end
+      scope '/channels' do
+        resources :api_channel, path: '/', only: [:create, :update]
+      end
+      scope '/messages' do
+        resources :api_message, path: '/', only: [:create, :update]
       end
       scope '/users' do
         resources :api_user, path: '/', only: [:create, :update]
-        scope '/memberships' do
-          resources :api_membership, path: '/', only: [:create, :update]
-        end
+      end
+      scope '/memberships' do
+        resources :api_membership, path: '/', only: [:create, :update]
       end
     end
   end
