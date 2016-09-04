415c4374f85ba1b39a7e0737b98279b4f95d3f2c
Jonathan B <greenbigfrog@gmail.com>
2016-09-04 11:19:49 +0200

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,18 @@ Rails.application.routes.draw do
   scope '/api' do
     scope '/v1' do
       scope '/servers' do
-        resources :api_server, only: [:new, :update]
+        resources :api_server, only: [:create, :update]
         scope '/channels' do
-          resources :api_channel, only: [:new, :update]
+          resources :api_channel, only: [:create, :update]
           scope '/messages' do
-            resources :api_message, only: [:new, :update]
+            resources :api_message, only: [:create, :update]
           end
         end
       end
       scope '/users' do
-        resources :api_user, only: [:new, :update]
+        resources :api_user, only: [:create, :update]
         scope '/memberships' do
-          resources :api_membership, only: [:new, :update]
+          resources :api_membership, only: [:create, :update]
         end
       end
     end
