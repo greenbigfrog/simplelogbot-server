16a36ce18cff2678f8230c80d0b09e5c8e898d05
Jonathan B <greenbigfrog@gmail.com>
2016-09-23 19:15:46 +0200

    raise error if no user_id got passed in

--- app/models/discord/user.rb
+++ app/models/discord/user.rb
@@ -7,6 +7,7 @@ module Discord
     self.primary_key = 'user_id'
 
     def self.ensure_user(user_id)
+      raise 'pass in a user ID' unless user_id
       @user = Discord::User.find_by(user_id: user_id)
       #  We need to make sure that a User exists in our database, before adding a message to our DB
       unless @user
